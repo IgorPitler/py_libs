33884a5069f32a1ba7c9d4258a942ff87b1e24df
Igor Pitler <igor.pitler.work@gmail.com>
2026-02-17 18:03:17 +0700

    py_serial_lib.py version 1.06

--- py_serial_lib.py
+++ py_serial_lib.py
@@ -1,4 +1,5 @@
-# version 1.05
+# version 1.06
+# by Igor Pitler
 # uses pyserial
 # install command: sudo apt install python3-serial
 import serial
@@ -9,6 +10,15 @@ class SerialDevice :
     timeout = 1
     encoding = "utf-8"
     # serial_dev
+
+    # 0 no error, 1 timeout, 2 common serial error
+    err_code = 0
+
+    def get_error(self):
+        return self.err_code
+
+    def set_error(self, code):
+        self.err_code = code
 
     def __init__(self, port="/dev/ttyUSB0", baudrate=9600, timeout=3) :
         self.port = port
@@ -21,7 +31,11 @@ class SerialDevice :
                                         parity=serial.PARITY_NONE,
                                         stopbits=serial.STOPBITS_ONE,
                                         timeout=self.timeout)
+        except serial.SerialTimeoutException as te:
+            self.set_error(1)
+            print("Serial timeout exception!")
         except serial.SerialException as e:
+            self.set_error(2)
             print("Serial exception!")
 
     def close(self):
@@ -29,19 +43,25 @@ class SerialDevice :
 
     def send(self, str_data):
         command = str_data.encode(self.encoding)
-
+        self.set_error(0)
         try:
             self.serial_dev.write(command)
         except serial.SerialTimeoutException as et:
+            self.set_error(1)
             print("Serial timeout exception!")
         except serial.SerialException as e:
+            self.set_error(2)
             print("Serial exception!")
 
     def get_response(self):
         response=""
+        self.set_error(0)
         try:
             response=self.serial_dev.readline().decode(self.encoding).strip() #arduino finish string with \n
+        except serial.SerialTimeoutException as te:
+            self.set_error(1)
+            print("Serial timeout exception!")
         except serial.SerialException as e:
-            # tmp
+            self.set_error(2)
             print("Serial exception!")
         return response
